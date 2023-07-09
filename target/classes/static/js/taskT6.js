35e6e2fd6867fab65bca4c1a82fb01b947132f35
StefT2800 <101137785+StefT2800@users.noreply.github.com>
2023-07-09 18:13:15 +0200

    Delete target directory

--- target/classes/static/js/taskT6.js
+++
@@ -1,9 +0,0 @@
-/*Implementazione fittizia della funzione la cui é interfaccia é stata concordata con il gruppo G08.
-Tale funzione accetta in ingresso una stringa Json, contenente le informazioni della partita necessarie 
-per le elaborazioni del gruppo G08. L'implementazione fittizia qui riportata stampa sulla console del
-browser la stringa Json fornita in ingresso. */  
-function taskT6(gsonObject) {
-  console.log(gsonObject) //DEBUG
-}
-
-export {taskT6}; //tale funzione viene esportata poiché utilizzata in un altro file javascript.
